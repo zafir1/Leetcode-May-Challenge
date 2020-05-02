5cd667dac710d483e61d79349334798438dd00ee
zafir <zafir@lyceuminc.com>
2020-05-03 00:02:15 +0530

    Day - 2 | Jewels and stones.

---
+++ 2_jewels_and_stones.cpp
@@ -0,0 +1,49 @@
+/*====================================================
+=            Question : Jewels and stones            =
+====================================================*/
+/**
+You're given strings J representing the types of stones that are jewels, and S representing the stones you have.  Each character in S is a type of stone you have.  You want to know how many of the stones you have are also jewels.
+
+The letters in J are guaranteed distinct, and all characters in J and S are letters. Letters are case sensitive, so "a" is considered a different type of stone from "A".
+
+Example 1:
+
+Input: J = "aA", S = "aAAbbbb"
+Output: 3
+Example 2:
+
+Input: J = "z", S = "ZZ"
+Output: 0
+Note:
+
+S and J will consist of letters and have length at most 50.
+The characters in J are distinct.
+
+*/
+
+/*=====  End of Question : Jewels and stones  ======*/
+
+
+/*=================================
+=            Solution             =
+=================================*/
+
+class Solution {
+public:
+    int numJewelsInStones(string J, string S) {
+        vector<char>v(256);
+        for(char &ch:J){
+            ++v[ch];
+        }
+        int ans = 0;
+        for(char &ch:S){
+            if(v[ch])
+            {
+                ++ans;
+            }
+        }
+        return ans;
+    }
+};
+
+/*=====  End of Solution   ======*/

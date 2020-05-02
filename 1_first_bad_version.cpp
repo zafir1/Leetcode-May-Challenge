a91d85867b482125c1aec10c4c77ed7f781f9faf
zafir <zafir@lyceuminc.com>
2020-05-02 08:31:46 +0530

    Day - 1

---
+++ 1_first_bad_version.cpp
@@ -0,0 +1,56 @@
+/*====================================================
+=            Question : First Bad version            =
+====================================================*/
+
+/*
+You are a product manager and currently leading a team to develop a new product. Unfortunately, the latest version of your product fails the quality check. Since each version is developed based on the previous version, all the versions after a bad version are also bad.
+
+Suppose you have n versions [1, 2, ..., n] and you want to find out the first bad one, which causes all the following ones to be bad.
+
+You are given an API bool isBadVersion(version) which will return whether version is bad. Implement a function to find the first bad version. You should minimize the number of calls to the API.
+
+Example:
+
+Given n = 5, and version = 4 is the first bad version.
+
+call isBadVersion(3) -> false
+call isBadVersion(5) -> true
+call isBadVersion(4) -> true
+
+Then 4 is the first bad version. 
+ */
+
+/*=====  End of Question : First Bad version  ======*/
+
+
+/*====================================================
+=            Solution : First Bad version            =
+====================================================*/
+
+// The API isBadVersion is defined for you.
+// bool isBadVersion(int version);
+
+class Solution {
+public:
+    int firstBadVersion(int n) {
+        int left = 1;
+        int right = n;
+        int ans = INT_MAX;
+        
+        while(left<=right){
+            int mid = left + (right-left)/2;
+            bool bad = isBadVersion(mid);
+            if(!bad){
+                left = mid + 1;
+            }else{
+                ans = min(ans,mid);
+                right = mid-1;
+            }
+        }
+        return ans;
+    }
+};
+
+/*=====  End of Solution : First Bad version  ======*/
+
+
